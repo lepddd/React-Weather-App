845b1949904276362cfc53560a1ab5de569a9a25
Luiz Dornelas <luizeduardojf@hotmail.com>
2022-09-16 19:15:30 -0300

    update components

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,21 +1,31 @@
-const WeatherCard = ({ temp, day, now }) => {
+import { useEffect, useState } from "react";
+
+const WeatherCard = ({ temp, day, icon, now }) => {
+  const [weekDay, setWeekDay] = useState("");
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  const splited = day.split("-");
+  function getWeekDay() {
+    const splitedDate = day.split("-");
+    return days[new Date(splitedDate).getDay()];
+  }
 
-  const weekDay = days[new Date(splited).getDay()];
+  useEffect(() => {
+    setWeekDay(getWeekDay());
+  }, []);
 
   return (
     <div
-      className={`relative flex px-11 py-5 justify-between items-center rounded ${
+      className={`relative flex justify-between items-center rounded ${
         now
-          ? "py-11 px-6 bg-gradient-to-b from-neutral-800 to-neutral-900 col-span-2 h-fit self-end sm:col-span-1 sm:row-span-2 sm:h-full sm:flex-col"
-          : "flex-col bg-neutral-50 gap-3.5 justify-self-center"
+          ? "py-11 px-6 bg-gradient-to-b from-neutral-800 to-neutral-900 col-span-2 h-fit self-end sm:col-span-1 sm:row-span-2 sm:h-full sm:flex-col md:py-14 md:px-8 transition-all"
+          : "px-11 py-5 flex-col bg-neutral-50 gap-3.5 justify-self-center md:px-14 md:py-7 transition-all"
       }`}
     >
-      {now && 
-      <p className="absolute top-3 left-3 flex text-xs font-bold text-neutral-50 p-1 rounded bg-blue-500 ">NOW</p>
-      }
+      {now && (
+        <p className="absolute top-3 left-3 flex text-xs font-bold text-neutral-50 p-1 rounded bg-blue-500 ">
+          NOW
+        </p>
+      )}
       <p
         className={`font-sans  ${
           now ? "text-3xl text-neutral-50" : "text-xl text-neutral-400"
@@ -23,13 +33,17 @@ const WeatherCard = ({ temp, day, now }) => {
       >
         {weekDay}
       </p>
-      <img className={` ${now ?'w-12 h-12 sm:h-16 sm:w-16': 'w-12 h-12'}`} src="./icons/medium/10.png" alt="icon" />
+      <img
+        className={` ${now ? "w-12 h-12 sm:h-16 sm:w-16" : "w-12 h-12"}`}
+        src={`./icons/medium/${icon}.png`}
+        alt="icon"
+      />
       <p
         className={`font-sans  ${
           now ? "text-3xl text-neutral-50" : "text-xl text-neutral-400"
         }`}
       >
-        {temp}
+        {temp}°C
       </p>
     </div>
   );
--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import WeatherCard from "./WeatherCard";
+import axios from "axios";
 const WeatherContainer = () => {
+  const url = "http://hide-heroku-api.herokuapp.com/weather2?city=juiz de fora";
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(url);
+      const response = res.data.daily.data;
+      const data = response.map((element, index) => {
+        return {
+          now: index === 0 ? true : false,
+          day: element.day,
+          temp: Math.floor(element.all_day.temperature),
+          icon: element.icon,
+        };
+      });
+      setData(data);
+    }
+    
+    getData();
+  }, []);
+
   return (
     <div className="grid grid-cols-2 grid-rows-4 gap-1 w-fit sm:grid-cols-4 sm:grid-rows-2">
-      <WeatherCard day={"2022-09-14"} temp={"16°C"} now={true} />
-      <WeatherCard day={"2022-09-15"} temp={"16°C"} now={false} />
-      <WeatherCard day={"2022-09-16"} temp={"16°C"} now={false} />
-      <WeatherCard day={"2022-09-17"} temp={"16°C"} now={false} />
-      <WeatherCard day={"2022-09-18"} temp={"16°C"} now={false} />
-      <WeatherCard day={"2022-09-19"} temp={"16°C"} now={false} />
-      <WeatherCard day={"2022-09-20"} temp={"16°C"} now={false} />
+      {data.map(({ day, temp, icon, now }, index) => (
+        <WeatherCard key={index} day={day} temp={temp} now={now} icon={icon} />
+      ))}
     </div>
   );
 };
